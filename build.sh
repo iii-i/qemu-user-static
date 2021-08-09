f5b76c4276de0207b96ebb5f572713438fee0601
Ilya Leoshkevich <iii@linux.ibm.com>
2021-08-09 14:15:39 +0200

    feat: qemu 6.1.0-rc2-867
    Usage:
    
        ./build.sh
        rm releases/usr/bin/*.tar.gz
        ./run.sh -d iiilinuxibmcom/qemu-user-static -t 6.1.0-rc2-867
        docker run --rm --privileged iiilinuxibmcom/qemu-user-static:6.1.0-rc2-867 --reset -p yes

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e -u -x
+cd "$(dirname "$0")"/qemu
+./configure --prefix=/usr --static --disable-system --enable-linux-user --enable-capstone=git
+make -j "$(nproc)"
+destdir=$PWD/../releases
+rm -rf "$destdir"
+make install DESTDIR="$destdir"
+for x in "$destdir"/usr/bin/qemu-*; do
+    mv "$x" "$x"-static
+done
